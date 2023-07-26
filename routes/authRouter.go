b629dc1f752e9774ee99a28d7f8a1d80190dd26f
Harsh Raj <harsh.smartters@gmail.com>
2023-07-26 13:25:59 +0530

    user Router and auth router

---
+++ routes/authRouter.go
@@ -0,0 +1,11 @@
+package routes
+
+import (
+	controller "github.com/saketharshraj/jwt-authentication-golang/controller"
+	"github.com/gin-gonic/gin"
+)
+
+func AuthRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.POST("users/signup", controller.Signup())
+	incomingRoutes.POST("users/login", controller.Login())
+}
